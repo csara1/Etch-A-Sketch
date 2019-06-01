537acbb0e6ea36df4878b3c1bc98dbd3eaf28be8
Charalampos (Babis) Sarantoglou <babis.sarantoglou@gmail.com>
2019-06-01 19:01:25 +0300

    Make a new grid when the button is clicked

--- code.js
+++ code.js
@@ -1,7 +1,7 @@
 function positionContainer() {
     let buttonHeight = button.offsetHeight,
-        windowHeight = window.innerHeight,
-        side = Math.min(window.innerWidth, windowHeight-buttonHeight);
+        windowHeight = window.innerHeight;
+    side = Math.min(window.innerWidth, windowHeight-buttonHeight);
     button.style.marginTop = (windowHeight-side-buttonHeight)/2 + "px";
     containerStyle.width = containerStyle.height = side + "px";
 }
@@ -12,22 +12,41 @@ function onmouseover(event) {
     itemStyle.borderColor = "black";
 }
 
+function addSquares() {
+    containerStyle.gridTemplateColumns = "repeat(" + squaresPerSide + ", 1fr)";
+    containerStyle.gridTemplateRows = "repeat(" + squaresPerSide + ", 1fr)";    
+    for(i=0; i<squaresPerSide*squaresPerSide; i++) {
+        let item = document.createElement('div');
+        item.classList.add("item");
+        item.id = 'item' + i;
+        item.addEventListener("mouseover", onmouseover);
+        container.appendChild(item);
+    }    
+}
+
+function reset() {
+    for(i=0; i<squaresPerSide*squaresPerSide; i++) {
+        let item = document.getElementById('item' + i);
+        item.removeEventListener("mouseover", onmouseover);
+    }
+    container.innerHTML = "";
+
+    do {
+        maxSquaresPerSide = Math.floor(side/6);
+        squaresPerSide = parseInt(window.prompt("Squares per side (1..." + maxSquaresPerSide + "): ", "16"));
+    } while (squaresPerSide <= 0 || squaresPerSide > maxSquaresPerSide);
+    
+    addSquares();
+}
+
 let button = document.getElementById('reset'),
     container = document.getElementById('container'),
     containerStyle = container.style,
+    side = 0,
     squaresPerSide = 16;
 
 positionContainer();
+button.addEventListener("click", reset);
 window.addEventListener("resize", positionContainer);
 
-containerStyle.gridTemplateColumns = "repeat(" + squaresPerSide + ", 1fr)";
-containerStyle.gridTemplateRows = "repeat(" + squaresPerSide + ", 1fr)";
-
-for(i=0; i<squaresPerSide*squaresPerSide; i++) {
-    container.innerHTML += '<div class="item" id="item' + i + '"></div>';
-}
-
-for(i=0; i<squaresPerSide*squaresPerSide; i++) {
-  let item = document.getElementById('item' + i);
-  item.addEventListener("mouseover", onmouseover);
-}
+addSquares();
