23149a6ad6ccd8ffc2a87e9d87e52286c5df911b
Charalampos (Babis) Sarantoglou <babis.sarantoglou@gmail.com>
2019-06-01 12:07:07 +0300

    Turn container into a 16x16 grid of square divs

--- code.js
+++ code.js
@@ -1,9 +1,21 @@
 function onresize() {
-  side = Math.min(window.innerWidth, window.innerHeight)+"px";
-  container.style.width = container.style.height = side;
+  side = Math.min(window.innerWidth, window.innerHeight) + "px";
+  containerStyle.width = containerStyle.height = side;
 }
 
 let container = document.getElementById('container'),
-    side = Math.min(window.innerWidth, window.innerHeight)+"px";
-container.style.width = container.style.height = side;
+    containerStyle = container.style,
+    side = Math.min(window.innerWidth, window.innerHeight) + "px",
+    squaresPerSide = 16;
+
+containerStyle.width = containerStyle.height = side;
 window.addEventListener("resize", onresize);
+
+containerStyle.gridTemplateColumns = "repeat(" + squaresPerSide + ", 1fr)";
+containerStyle.gridTemplateRows = "repeat(" + squaresPerSide + ", 1fr)";
+for(i=0; i<squaresPerSide; i++) {
+    for(j=0; j<squaresPerSide; j++) {
+        container.innerHTML += (i+j)&1 ? '<div class="odd"></div>'
+                                      : '<div class="even"></div>';
+    }
+}
